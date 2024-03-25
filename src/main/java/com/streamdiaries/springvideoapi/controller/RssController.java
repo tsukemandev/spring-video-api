123140a2953e8c9d63b9582ee626b0ba6287be7e
tsukemendog <mokaim@outlook.com>
2024-03-25 21:50:39 +0900

    cors 로컬개발용 오리진 추가

--- src/main/java/com/streamdiaries/springvideoapi/controller/RssController.java
+++ src/main/java/com/streamdiaries/springvideoapi/controller/RssController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,7 @@ import com.streamdiaries.springvideoapi.entity.Rss;
 import com.streamdiaries.springvideoapi.repository.RssRepository;
 
 
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class RssController {
 
